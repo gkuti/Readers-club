133897443f2e5d7b0b0e153d576fcec792160fcb
andela-gkuti <kuti.gbolahan@andela.com>
2016-02-25 17:10:05 +0100

    create Student, Staff, Member and Book classes

---
+++ src/com/andela/gkuti/library/Member.java
@@ -0,0 +1,35 @@
+package com.andela.gkuti.library;
+	import java.util.Date;
+public class Member {
+    private String name;
+    private String address;
+    private Date registrationDate;
+    private int memberId;
+
+    public Member(String name, String address,int memberId) {
+        this.name = name;
+        this.address = address;
+        this.memberId = memberId;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public int getMemberid() {
+        return memberId;
+    }
+
+    }
---
+++ src/com/andela/gkuti/library/Student.java
@@ -0,0 +1,28 @@
+package com.andela.gkuti.library;
+
+public class Student extends Member {
+	private String subjectOfInterest;
+	private String institution;
+	private String department;
+
+	public Student(String name, String address, int memberId, String institution, String subjectOfInterest, String department) {
+		super(name, address, memberId);
+		this.institution = institution;
+		this.subjectOfInterest = subjectOfInterest;
+		this.department = department;
+		
+	}
+
+	public String getSubjectOfInterest() {
+		return subjectOfInterest;
+	}
+
+	public String getInstitution() {
+		return institution;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+}
