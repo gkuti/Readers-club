2b7c508c020d2acd0c828b36e049e6cf2291ff1d
andela-gkuti <kuti.gbolahan@andela.com>
2016-02-25 16:35:55 +0100

    add assert statement to LibraryTest

--- test/com/andela/gkuti/library/test/LibraryTest.java
+++ test/com/andela/gkuti/library/test/LibraryTest.java
@@ -3,41 +3,111 @@ package com.andela.gkuti.library.test;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 import static org.junit.Assert.*;
 
 public class LibraryTest {
-
+    Book book1,book2,book3,book4;
+    Member member1,member2,member3;
+    Library library;
+    PriorityQueue<Member> bookQueue;
+    BookQueueComparator bookQueueComparator;
     @Before
-    public void setUp() {
-
+    public void setUp() throws Exception {
+        library = new Library();
+        member1 = new Student("Kuti Gbolahan", "21 onabola street, somolu", 1, "Laspotech", "Computer Science", "Science");
+        member2 = new Staff("Kayode Olowu", "1 boyega street, sogunle", 2, 45000, 9, "Accounting");
+        member3 = new Student("Samuel Okonkwo", "37 calabar street, masha", 3, "Laspotech", "History", "Art");
+        book1 = new Book("Harry Porter", "978-0439139601", "JK Rolins", 2);
+        book2 = new Book("The 3 Musketeers", "978-1853260407", "Alexandre Dumas", 1);
+        book3 = new Book("Spatial Mathematics", "978-1466505322", "Sandra Lach A.", 0);
+        book4 = new Book("The Trials of Brother Jero and The Strong Breed", "978-0822210900", "Wole Soyinka", 4);
+        bookQueueComparator = new BookQueueComparator();
+        bookQueue = new PriorityQueue<Member>(bookQueueComparator);
     }
 
     @Test
-    public void testBorrowBook() {
-
+    public void testBorrowBook() throws Exception {
+        library.registerMember(member1);
+        library.registerMember(member2);
+        library.registerMember(member3);
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        assertEquals("expect to return 2", 2, library.borrowBook(book1, member1,member2,member3));
+        assertEquals("expect to return 1", 1, library.borrowBook(book2, member1,member2,member3));
+        assertEquals("expect to return 0", 0, library.borrowBook(book3, member2,member3));
     }
 
     @Test
-    public void testRequest() {
-
+    public void testRequest() throws Exception {
+        library.registerMember(member1);
+        library.registerMember(member2);
+        library.registerMember(member3);
+        library.addBook(book1);
+        library.addBook(book2);
+        assertEquals("expect to return 2", 2, library.request(book1, member1,member2,member3));
+        assertEquals("expect to return 1", 1, library.request(book2, member1,member2,member3));
+        book2 = new Book("The 3 Musketeers", "978-1853260407", "Alexandre Dumas", 1);
+        assertEquals("expect to return 1", 1, library.request(book2, member2,member3));
     }
 
     @Test
-    public void testAcceptedRequest() {
-
+    public void testAcceptedRequest() throws Exception {
+        library.registerMember(member1);
+        library.registerMember(member2);
+        library.registerMember(member3);
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        bookQueue.add(member1);
+        bookQueue.add(member2);
+        bookQueue.add(member3);
+        assertEquals("expect to return 2", 2, library.acceptedRequest(book1, bookQueue, member1,member2,member3));
+        assertEquals("expect to return 1", 1, library.acceptedRequest(book2, bookQueue, member1,member2,member3));
+        book2 = new Book("The 3 Musketeers", "978-1853260407", "Alexandre Dumas", 0);
+        bookQueue.add(member1);
+        bookQueue.add(member2);
+        bookQueue.add(member3);
+        assertEquals("expect to return 0", 0, library.acceptedRequest(book2, bookQueue, member1));
+        bookQueue.add(member1);
+        bookQueue.add(member2);
+        bookQueue.add(member3);
+        assertEquals("expect to return 3", 3, library.acceptedRequest(book4, bookQueue, member1,member2,member3));
     }
 
     @Test
-    public void testBorrower() {
-
+    public void testBorrower() throws Exception {
+        library.registerMember(member1);
+        library.registerMember(member2);
+        library.registerMember(member3);
+        library.addBook(book1);
+        book1 = new Book("Harry Porter", "978-0439139601", "JK Rolins", 2);
+        library.borrowBook(book1, member1,member2,member3);
+        assertEquals("expect borrower to be student object", member1, library.borrower(0));
+        book1 = new Book("Harry Porter", "978-0439139601", "JK Rolins", 2);
+        library.borrowBook(book1, member1,member2,member3);
+        assertEquals("expect borrower to be staff object", member2, library.borrower(1));
     }
 
     @Test
-    public void testGetBorrowersList() {
-
+    public void testGetBorrowersList() throws Exception {
+        library.registerMember(member1);
+        library.registerMember(member2);
+        library.registerMember(member3);
+        library.addBook(book1);
+        library.borrowBook(book1, member1,member2,member3);
+        ArrayList<Member> bookList = library.getBorrowersList();
+        assertEquals("expect borrower to be member1", member1, bookList.get(0));
+        assertEquals("expect borrower to be member2", member2, bookList.get(1));
+        assertEquals("expect to return", 2, bookList.size());
     }
 
     @Test
-    public void testBookAndBorrowers()
+    public void testBookAndBorrowers() throws Exception {
+
     }
+
 }
