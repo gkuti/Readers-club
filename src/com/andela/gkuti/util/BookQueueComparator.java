f0d7ef9c9d821904c7f144c77dfc99e73af73798
andela-gkuti <kuti.gbolahan@andela.com>
2016-03-03 11:20:42 +0100

    Modify BookQueueComparator

--- src/com/andela/gkuti/util/BookQueueComparator.java
+++ src/com/andela/gkuti/util/BookQueueComparator.java
@@ -15,26 +15,34 @@ public class BookQueueComparator implements Comparator<Member> {
     public int compare(Member member1, Member member2) {
         if (member1 instanceof Student && member2 instanceof Staff) {
             return -1;
-        } else if (member1 instanceof Staff && member2 instanceof Student) {
+        }
+
+        if (member1 instanceof Staff && member2 instanceof Student) {
             return 1;
-        } else
-            return compareTime(member1, member2);
+        }
+
+        return compareTime(member1, member2);
     }
 
     public int compareTime(Member member1, Member member2) {
         int priority = 0;
-        if (member1 instanceof Staff && member2 instanceof Staff && member1.getRegistrationDate().compareTo(member2.getRegistrationDate()) > 0) {
+        if (member1 instanceof Staff && member2 instanceof Staff && compareMembersByDate(member1, member2) > 0) {
             priority = -1;
         }
-        if (member1 instanceof Staff && member2 instanceof Staff && member1.getRegistrationDate().compareTo(member2.getRegistrationDate()) < 0) {
+        if (member1 instanceof Staff && member2 instanceof Staff && compareMembersByDate(member1, member2) < 0) {
             priority = 1;
         }
-        if (member1 instanceof Student && member2 instanceof Student && member1.getRegistrationDate().compareTo(member2.getRegistrationDate()) > 0) {
+        if (member1 instanceof Student && member2 instanceof Student && compareMembersByDate(member1, member2) > 0) {
             priority = -1;
         }
-        if (member1 instanceof Student && member2 instanceof Student && member1.getRegistrationDate().compareTo(member2.getRegistrationDate()) < 0) {
+        if (member1 instanceof Student && member2 instanceof Student && compareMembersByDate(member1, member2) < 0) {
             priority = 1;
         }
         return priority;
+    }
+
+    public int compareMembersByDate(Member member1, Member member2) {
+        return member1.getRegistrationDate().compareTo(member2.getRegistrationDate());
+
     }
 }
